3ec484b64065025a29a9179741819bfa5e5ba3ee
jehanusama <jehanusama3@gmail.com>
2025-12-22 18:53:07 +0200

    add .env file

--- src/router/index.js
+++ src/router/index.js
@@ -283,7 +283,7 @@ const routes = [
   {
     path: '/dashboard',
     component: DashboardLayout,
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: false },
     children: [
       {
         path: 'overview',
